5ae23b18c67ffcdf8d6eb32227b7bc9653597f8d
MrAmoako <simplecraft1212@gmail.com>
2024-08-28 20:59:56 -0700

    image

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+    <h1>AI Image Generator</h1>
+    <div className='Card'>
+         
+    </div>
+      <p>
+        Powered By OpenAI
+      </p>
+      </div>
+    </>
+  )
+}
+
+export default App
