ec1139a270cff567441e686f820a2866bfa3a331
MrAmoako <simplecraft1212@gmail.com>
2024-08-28 21:30:08 -0700

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
+import { Configuration, OpenAIApi} from 'openai'
 import './App.css'
 
 function App() {
-
+  const [prompt, setPrompt] = useState('')
+  const [result, setResult] = useState('')
+  const [loading, setLoading] = useState(false)
+   
+  const Configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  })
+    
+  const openai = new OpenAIApi(Configuration);
+   
   return (
     <>
     <div className='App'>
